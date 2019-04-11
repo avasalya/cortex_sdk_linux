0c6e5c31ccb2e92b54bb9d37687e7c03fb42927b
ashesh vasalya <ashesh@gite.lirmm.fr>
2019-04-11 18:41:09 +0900

    working on HRP4

--- README.txt
+++ README.txt
@@ -7,15 +7,12 @@ Linux version of Cortex SDK
 ===========================
 
 Compile this example code using the commands
-
-mkdir build 
-cd build 
-cmake .
-make
+  cmake .
+  make
 
 You will get a static library 'libcortex_sdk.a' and a test binary 'clienttest'.
 As Cortex streams multi-cast data on port 1001, you need to be root to receive multi-cast data, e.g. by running
-
-sudo ./clienttest
+  sudo ./clienttest
 
 Tested with Ubuntu 10.04
+
---
+++ clientTest2.cpp
@@ -0,0 +1,108 @@
+/*=========================================================
+ //
+ // File: ClientTest.cpp
+ //
+ // Created by Ned Phipps, Oct-2004
+ //
+ =============================================================================*/
+
+#include <string.h>
+#include <stdio.h>
+#include <iostream>
+
+#include <unistd.h>
+
+#include "cortex.h"
+
+void MyErrorMsgHandler(int iLevel, const char *szMsg)
+{
+  const char *szLevel = NULL;
+
+  if (iLevel == VL_Debug) {
+    szLevel = "Debug";
+  } else if (iLevel == VL_Info) {
+    szLevel = "Info";
+  } else if (iLevel == VL_Warning) {
+    szLevel = "Warning";
+  } else if (iLevel == VL_Error) {
+    szLevel = "Error";
+  }
+  printf("  %s: %s\n", szLevel, szMsg);
+}
+
+
+int main()
+{
+
+  sBodyDefs* pBodyDefs = NULL;
+  sFrameOfData* FrameofData = NULL;
+
+  int retval = RC_Okay;
+
+  Cortex_SetVerbosityLevel(VL_Info);
+  Cortex_SetErrorMsgHandlerFunc(MyErrorMsgHandler);
+  
+  retval = Cortex_Initialize("10.1.1.180", "10.1.1.100");
+
+  if (retval != RC_Okay)
+  {
+    printf("Error: Unable to initialize ethernet communication\n");
+    retval = Cortex_Exit();
+    return 1;
+  }
+  
+  printf("\n****** Cortex_GetBodyDefs ******\n");
+
+  pBodyDefs = Cortex_GetBodyDefs();
+  if (pBodyDefs == NULL) 
+  {
+    printf("Failed to get body defs\n");
+  } 
+  else 
+  {
+    printf("number of markers defined in body1: %d\n",pBodyDefs->BodyDefs[0].nMarkers);
+    printf("number of markers defined in body2:  %d\n",pBodyDefs->BodyDefs[1].nMarkers);
+  }
+
+  void *pResponse;
+  int nBytes;
+  retval = Cortex_Request("GetContextFrameRate", &pResponse, &nBytes);
+  if (retval != RC_Okay)
+    printf("ERROR, GetContextFrameRate\n");
+  float *contextFrameRate = (float*) pResponse;
+  printf("ContextFrameRate = %3.1f Hz\n", *contextFrameRate);
+
+
+  printf("\n*** Starting live mode ***\n");
+  
+  retval = Cortex_Request("LiveMode", &pResponse, &nBytes);
+  int bodyStickMarkers =  pBodyDefs->BodyDefs[0].nMarkers;
+  Cortex_FreeBodyDefs(pBodyDefs);
+  
+  while(1)
+  {
+    FrameofData =  Cortex_GetCurrentFrame();  // Can POLL for the current frame. 
+
+    for(int m = 0; m<bodyStickMarkers; m++)
+    {
+      // usleep(100000);  
+      printf("time delay from camera %f  cortex frame %d  body1 markers %d == x: %f y: %f z: %f\n", 
+                      FrameofData->fDelay, 
+                      FrameofData->iFrame, 
+                      (m+1),
+                      FrameofData->BodyData[0].Markers[m][0],
+                      FrameofData->BodyData[0].Markers[m][1],
+                      FrameofData->BodyData[0].Markers[m][2]
+                      );
+    }
+    // usleep(1000000);
+
+    if(FrameofData->iFrame == 1000)
+    {
+      printf("\n****** Paused live mode ... exiting Cortex ******\n");
+      retval = Cortex_Request("Pause", &pResponse, &nBytes);
+      retval = Cortex_Exit();
+    }
+  } 
+  return 0;
+}
